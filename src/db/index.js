f20cceb296f89251dd9ba742c1b4409bcef7662a
testgithubtiwari <rt936649@gmail.com>
2024-08-09 15:01:28 +0530

    database connection done

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export let dbInstance = undefined;
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@software1.gptczdh.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&appName=software1`
+    );
+    dbInstance = connectionInstance;
+    console.log(
+      `\n☘️  MongoDB Connected! Db host: ${connectionInstance.connection.host}\n`
+    );
+  } catch (error) {
+    console.log("MongoDB connection error: ", error);
+    process.exit(1);
+  }
+};
+
+export { connectDB };
---
+++ src/server.js
@@ -0,0 +1,66 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import http from "http";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(cors({ origin: "*", methods: ["GET", "POST"], credentials: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET || "your-secret-key",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+const HTTP_PORT = process.env.HTTP_PORT || 80;
+
+app.get("*", (req, res) => {
+  res.json({
+    message: "Wecome to BTP api's",
+  });
+});
+
+import { connectDB } from "./db/index.js";
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    const httpServer = http.createServer(app);
+
+    const ioHttp = new Server(httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    httpServer.listen(HTTP_PORT, () => {
+      console.log(`HTTP Server running on http://localhost:${HTTP_PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();

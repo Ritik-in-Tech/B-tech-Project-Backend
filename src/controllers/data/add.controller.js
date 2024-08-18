70cec44a4f834c471abd88759bc2a9a76faf2dda
testgithubtiwari <rt936649@gmail.com>
2024-08-18 23:46:17 +0530

    start to working on add data

---
+++ src/controllers/data/add.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../../helpers/response/asynchandler";
+
+export const addData = asyncHandler(async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!userId) {
+    }
+
+    
+  } catch (error) {}
+});

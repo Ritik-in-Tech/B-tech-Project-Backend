6f24d2d5b72bae7103c372355feeb9ed13bc29e2
testgithubtiwari <rt936649@gmail.com>
2024-09-13 03:01:17 +0530

    changes in user routes done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { registerUser } from "../controllers/users/create.controller.js";
 import { loginUser } from "../controllers/users/login.controller.js";
 import { getAllUsers, getUser } from "../controllers/users/get.controller.js";
+import { verifyFinger } from "../controllers/users/verifyfinger.controller.js";
 
 const router = Router();
 
@@ -12,4 +13,6 @@ router.route("/login").post(loginUser);
 router.route("/").get(getAllUsers);
 
 router.route("/:rollNumber").get(getUser);
+
+router.route("/verify-finger").post(verifyFinger);
 export default router;

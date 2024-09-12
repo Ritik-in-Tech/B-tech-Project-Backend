34ac357db5653f32f3054b6e93be44e2f65936bf
testgithubtiwari <rt936649@gmail.com>
2024-09-13 03:01:03 +0530

    new routes added

--- src/routes/mess.routes.js
+++ src/routes/mess.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import { chooseMess } from "../controllers/mess/choose.controller.js";
+import { EntryDataMess } from "../controllers/mess/entry.mess.controller.js";
 
 const router = Router();
 
 router.route("/choose-mess/:userId").post(chooseMess);
+
+router.route("/entry-mess").post(EntryDataMess);
 
 export default router;

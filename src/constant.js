29f42fd9c202dacca9f2a7e3dffd2c7e6ba996bd
testgithubtiwari <rt936649@gmail.com>
2024-10-11 19:35:12 +0530

    email admin and mess email list added

--- src/constant.js
+++ src/constant.js
@@ -2,6 +2,18 @@ export const validMessNames = ["Old", "New"];
 
 export const validRoles = ["admin", "students", "mess"];
 
+export const validAdminEmail = [
+  "admin@iitj.ac.in",
+  "admin1@iitj.ac.in",
+  "admin2@iitj.ac.in",
+];
+
+export const validMessEmail = [
+  "mess@iitj.ac.in",
+  "mess1@iitj.ac.in",
+  "mess2@iitj.ac.in",
+];
+
 export const messTime = {
   Breakfast: {
     start: "07:30",

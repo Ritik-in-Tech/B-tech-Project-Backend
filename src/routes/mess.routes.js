afe6bec3fa227a1c217dfc380389a4b934d808e8
testgithubtiwari <rt936649@gmail.com>
2024-10-11 20:03:50 +0530

    jwt added in mess route

--- src/routes/mess.routes.js
+++ src/routes/mess.routes.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 
 import { chooseMess } from "../controllers/mess/choose.controller.js";
 import { EntryDataMess } from "../controllers/mess/entry.mess.controller.js";
+import { verifyJwt } from "../middlewares/auth_middleware.js";
 
 const router = Router();
 
-router.route("/choose-mess/:userId").post(chooseMess);
+router.use(verifyJwt);
+
+router.route("/choose-mess").post(chooseMess);
 
 router.route("/entry-mess").post(EntryDataMess);
 

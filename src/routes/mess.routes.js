7217eb8f06f4cd05a97b8cd105ad8a6cf91b2bfe
testgithubtiwari <rt936649@gmail.com>
2024-10-23 01:17:54 +0530

    new mess route controller made

--- src/routes/mess.routes.js
+++ src/routes/mess.routes.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { chooseMess } from "../controllers/mess/choose.controller.js";
 import { EntryDataMess } from "../controllers/mess/entry.mess.controller.js";
 import { verifyJwt } from "../middlewares/auth_middleware.js";
+import { GetMessData } from "../controllers/mess/get_mess_data.controller.js";
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.use(verifyJwt);
 router.route("/choose-mess").post(chooseMess);
 
 router.route("/entry-mess").post(EntryDataMess);
+
+router.route("/get-mess-data/:messName").get(GetMessData);
 
 export default router;

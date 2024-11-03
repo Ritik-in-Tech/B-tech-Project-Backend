c04bacfe5c9ce1773461f36d547de85b24ef244d
testgithubtiwari <rt936649@gmail.com>
2024-11-04 01:13:13 +0530

    mess route updated

--- src/routes/mess.routes.js
+++ src/routes/mess.routes.js
@@ -3,8 +3,10 @@ import { Router } from "express";
 import { chooseMess } from "../controllers/mess/choose.controller.js";
 import { EntryDataMess } from "../controllers/mess/entry.mess.controller.js";
 import { verifyJwt } from "../middlewares/auth_middleware.js";
-import { GetMessData } from "../controllers/mess/get_mess_data.controller.js";
+// import { GetMessData } from "../controllers/mess/get_mess_data.controller.js";
 import { entryMessQR } from "../controllers/mess/entry_mess_qr.controller.js";
+import { GetOverallMessData } from "../controllers/mess/data/overall_data.controller.js";
+import { getMessDataByDate } from "../controllers/mess/data/data_by_date.controller.js";
 
 const router = Router();
 
@@ -14,8 +16,10 @@ router.route("/choose-mess").post(chooseMess);
 
 router.route("/entry-mess").post(EntryDataMess);
 
-router.route("/get-mess-data/:messName").get(GetMessData);
+router.route("/get-mess-data/:messName").get(GetOverallMessData);
 
 router.route("/entry-mess-qr").post(entryMessQR);
+
+router.route("/mess-data-byDate/:date").get(getMessDataByDate);
 
 export default router;
